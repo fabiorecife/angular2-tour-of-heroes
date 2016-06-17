05cd38aa0a36ce6bfb4dd61f363f8c75c706c313
fabiorecife <fabiorecife@gmail.com>
2016-06-17 13:47:21 -0300

    angular rc2

--- systemjs.config.js
+++ systemjs.config.js
@@ -39,8 +39,8 @@
 
   // Bundled (~40 requests):
   function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-  };
+    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
 
   var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
 
@@ -50,7 +50,7 @@
   var config = {
     map: map,
     packages: packages
-  }
+  };
 
   System.config(config);
 
