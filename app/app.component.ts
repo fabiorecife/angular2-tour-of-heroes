fefffffe61f396110c16371905c8208901108269
fabiorecife <fabiorecife@gmail.com>
2016-07-15 12:12:23 -0300

    minor

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,14 +10,13 @@ import { HeroService }         from './hero.service';
     <h1>{{title}}</h1>
     <nav>
      <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
-    <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+     <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
     </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['app/app.component.css'],
    directives: [ROUTER_DIRECTIVES],
   providers: [
-   
     HeroService,
   ]
 })
